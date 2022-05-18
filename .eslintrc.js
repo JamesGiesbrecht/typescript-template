0f1a8d609e16d42f815e3e159ab147e798a67044
James Giesbrecht <giesbrechtjames@gmail.com>
2022-05-18 16:04:14 -0500

    Configuring eslint and some basic commands

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'node/no-unsupported-features/es-syntax': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'object-curly-newline': ['error', { ObjectPattern: { multiline: true } }],
+    semi: ['error', 'never'],
+    'prettier/prettier': [
+      'warn',
+      {
+        semi: false,
+        trailingComma: 'all',
+        singleQuote: true,
+        printWidth: 100,
+        parser: 'typescript',
+        tabWidth: 2,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
